252eae1c789afd21729331b09fe47ed480751017
mcornell74 <mcornell74@gmail.com>
2016-12-30 20:58:01 -0500

    Lombok wasn't generating Getters/Setters. Will sort out later

--- moviemogul/src/main/java/net/mcornell/moviemogul/domain/Actor.java
+++ moviemogul/src/main/java/net/mcornell/moviemogul/domain/Actor.java
@@ -6,9 +6,6 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import lombok.Data;
-
-@Data
 @Entity
 public class Actor {
 
@@ -41,4 +38,85 @@ public class Actor {
 		this.data6 = data6;
 
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public Byte getData0() {
+		return data0;
+	}
+
+	public void setData0(Byte data0) {
+		this.data0 = data0;
+	}
+
+	public Byte getData1() {
+		return data1;
+	}
+
+	public void setData1(Byte data1) {
+		this.data1 = data1;
+	}
+
+	public Byte getData2() {
+		return data2;
+	}
+
+	public void setData2(Byte data2) {
+		this.data2 = data2;
+	}
+
+	public Byte getData3() {
+		return data3;
+	}
+
+	public void setData3(Byte data3) {
+		this.data3 = data3;
+	}
+
+	public Byte getData4() {
+		return data4;
+	}
+
+	public void setData4(Byte data4) {
+		this.data4 = data4;
+	}
+
+	public Byte getData5() {
+		return data5;
+	}
+
+	public void setData5(Byte data5) {
+		this.data5 = data5;
+	}
+
+	public Byte getData6() {
+		return data6;
+	}
+
+	public void setData6(Byte data6) {
+		this.data6 = data6;
+	}
+
 }
