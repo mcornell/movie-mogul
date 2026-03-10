da40cce6af490914bdbea53c6a6e5a765a695f42
Mike Cornell <mcornell74@gmail.com>
2026-03-10 10:46:42 -0400

    Create src/game/gameEngine.ts with core game logic

---
+++ src/game/gameEngine.ts
@@ -0,0 +1,21 @@
+// core game logic
+
+class GameEngine {
+    constructor() {
+        this.state = {};
+    }
+
+    startGame() {
+        console.log('Game started!');
+    }
+
+    updateGame() {
+        // Logic to update game state
+    }
+
+    endGame() {
+        console.log('Game ended!');
+    }
+}
+
+export default GameEngine;
